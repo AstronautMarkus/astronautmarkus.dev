f800aa1a2f1343f16f132b60c393e8a61fe684e7
Marcos Reyes <astronautmarkus@gmail.com>
2026-03-04 17:21:07 -0300

    feat: remove halo.js script from base layout

--- website/app/static/js/halo.js
+++
@@ -1,7 +0,0 @@
-const HaloImage = document.querySelector('.halo-image');
-
-if (HaloImage) {
-    const haloLeftOffset = Math.random() * 30 + 20;
-
-    HaloImage.style.left = `${haloLeftOffset}px`;
-}
