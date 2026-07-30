b7cbbe83be450af02000dba916d5f396325afe33
Marcos Reyes <astronautmarkus@gmail.com>
2026-07-30 19:53:50 -0400

    feat: added about me website base.

--- website/app/routes/main/about.py
+++ website/app/routes/main/about.py
@@ -1,7 +1,6 @@
 from app.routes.main import main_bp
 from app import render_localized_template
 
-
-@main_bp.route('/about')
-def about():
+@main_bp.route('/about-me')
+def about_me():
     return render_localized_template('main/about.html')
